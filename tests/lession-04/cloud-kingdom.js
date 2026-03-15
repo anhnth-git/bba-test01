c2d579719f88d7076312df0199b0750072aa8ef3
test01-huonganh <test01@gmail.com>
2026-03-15 22:58:21 +0700

    feat: add solution for challenge 03

---
+++ tests/lession-04/cloud-kingdom.js
@@ -0,0 +1,8 @@
+let playerName = "Mario";
+let currentLives = 3;
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+const tong = level1 + level2 + level3;
+console.log(tong / 3);
+console.log(tong % 3);
