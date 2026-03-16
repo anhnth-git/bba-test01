5835959a9e706615f18f4d4750ceeafbe333dff0
test01-huonganh <test01@gmail.com>
2026-03-16 13:31:15 +0700

    bai 3

--- tests/lession-04/cloud-kingdom.js
+++ tests/lession-04/cloud-kingdom.js
@@ -1,1 +1,9 @@
-let 
+let playerName = "Mario";
+let currentLives = 3;
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+const tong = level1 + level2 + level3;
+console.log(tong / 3);
+console.log(tong % 3);
+console.log(tong+3);
