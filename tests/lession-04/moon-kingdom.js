9766f5df66e712b95bc9fcc37295f7b6c2e08368
test01-huonganh <test01@gmail.com>
2026-03-17 22:23:19 +0700

    bai 4

--- tests/lession-04/moon-kingdom.js
+++ tests/lession-04/moon-kingdom.js
@@ -1,23 +1,27 @@
-//Bài 1
-// let characters = [
-//     {name: "tuan", level: 1, health: 30000}, 
-//     {name: "anhnth", level: 9, health: 356}, 
-//     {name: "ngoc", level: 6, health: 1001}
-// ]
-// let charactersPowerUp = characters.map(function(characters){
-//     return {
-//         name :characters.name.toUpperCase(),
-//         level: characters.level*2,
-//         health: characters.health*3
-//     }
-// });
-// const possibleWinners = characters.filter(characters => characters.health >1000)
-// function createCharacters(){
-//     console.log(charactersPowerUp); 
-//     console.log(possibleWinners)
-// }
-// createCharacters(characters);
+// Bài 1
+
+let characters = [
+    { name: "tuan", level: 1, health: 30000 },
+    { name: "anhnth", level: 9, health: 356 },
+    { name: "ngoc", level: 6, health: 1001 }
+]
+let charactersPowerUp = characters.map(function (characters) {
+    return {
+        name: characters.name.toUpperCase(),
+        level: characters.level * 2,
+        health: characters.health * 3
+    }
+});
+const possibleWinners = characters.filter(characters => characters.health > 1000)
+function createCharacters() {
+    console.log(charactersPowerUp);
+    console.log(possibleWinners)
+}
+createCharacters(characters);
+
 // Bài 2
+
+
 let player = [
     { name: "Mario", score: 1000 },
     { name: "Hai", score: 4562 },
